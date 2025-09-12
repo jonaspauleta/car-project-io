4216d6387adda656a8f3a1ea3a81c2523cdb1fc8
João Paulo Santos <jonaspauleta2@gmail.com>
2025-09-12 19:21:50 +0100

    add rules for laravel pint model, migration, factory and tests for Car and Modifications solved issues with routes/web.php crashing npm run build

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ModificationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,44 +18,44 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::prefix('garage')
         ->group(function () {
-            Route::get('/', [GarageController::class, 'index'])->name('index');
-            Route::get('/{car}', [GarageController::class, 'show'])->name('show');
-            Route::get('/{car}/edit', [GarageController::class, 'edit'])->name('edit');
-            Route::post('/', [GarageController::class, 'store'])->name('store');
-            Route::put('/{car}', [GarageController::class, 'update'])->name('update');
-            Route::delete('/{car}', [GarageController::class, 'destroy'])->name('destroy');
+            Route::get('/', [CarController::class, 'index'])->name('cars.index');
+            Route::get('/{car}', [CarController::class, 'show'])->name('cars.show');
+            Route::get('/{car}/edit', [CarController::class, 'edit'])->name('cars.edit');
+            Route::post('/', [CarController::class, 'store'])->name('cars.store');
+            Route::put('/{car}', [CarController::class, 'update'])->name('cars.update');
+            Route::delete('/{car}', [CarController::class, 'destroy'])->name('cars.destroy');
 
             Route::prefix('{car}/mods')
                 ->group(function () {
-                    Route::get('/', [ModificationsController::class, 'index'])->name('index');
-                    Route::get('/{modification}', [ModificationsController::class, 'show'])->name('show');
-                    Route::get('/{modification}/edit', [ModificationsController::class, 'edit'])->name('edit');
-                    Route::post('/', [ModificationsController::class, 'store'])->name('store');
-                    Route::put('/{modification}', [ModificationsController::class, 'update'])->name('update');
-                    Route::delete('/{modification}', [ModificationsController::class, 'destroy'])->name('destroy');
+                    Route::get('/', [ModificationController::class, 'index'])->name('modifications.index');
+                    Route::get('/{modification}', [ModificationController::class, 'show'])->name('modifications.show');
+                    Route::get('/{modification}/edit', [ModificationController::class, 'edit'])->name('modifications.edit');
+                    Route::post('/', [ModificationController::class, 'store'])->name('modifications.store');
+                    Route::put('/{modification}', [ModificationController::class, 'update'])->name('modifications.update');
+                    Route::delete('/{modification}', [ModificationController::class, 'destroy'])->name('modifications.destroy');
                 })->name('mods.');
 
             /* TODO ideas for later
-        Route::prefix('{car}/maintenance')
-        ->group(function () {
-            Route::get('/', [MaintenanceController::class, 'index'])->name('index');
-            Route::get('/{maintenance}', [MaintenanceController::class, 'show'])->name('show');
-            Route::get('/{maintenance}/edit', [MaintenanceController::class, 'edit'])->name('edit');
-            Route::post('/', [MaintenanceController::class, 'store'])->name('store');
-            Route::put('/{maintenance}', [MaintenanceController::class, 'update'])->name('update');
-            Route::delete('/{maintenance}', [MaintenanceController::class, 'destroy'])->name('destroy');
-        })->name('maintenance.');
+            Route::prefix('{car}/maintenance')
+            ->group(function () {
+                Route::get('/', [MaintenanceController::class, 'index'])->name('index');
+                Route::get('/{maintenance}', [MaintenanceController::class, 'show'])->name('show');
+                Route::get('/{maintenance}/edit', [MaintenanceController::class, 'edit'])->name('edit');
+                Route::post('/', [MaintenanceController::class, 'store'])->name('store');
+                Route::put('/{maintenance}', [MaintenanceController::class, 'update'])->name('update');
+                Route::delete('/{maintenance}', [MaintenanceController::class, 'destroy'])->name('destroy');
+            })->name('maintenance.');
 
-        Route::prefix('{car}/setup')
-        ->group(function () {
-            Route::get('/', [SetupController::class, 'index'])->name('index');
-            Route::get('/{setup}', [SetupController::class, 'show'])->name('show');
-            Route::get('/{setup}/edit', [SetupController::class, 'edit'])->name('edit');
-            Route::post('/', [SetupController::class, 'store'])->name('store');
-            Route::put('/{setup}', [SetupController::class, 'update'])->name('update');
-            Route::delete('/{setup}', [SetupController::class, 'destroy'])->name('destroy');
-        })->name('setup.');
-        */
+            Route::prefix('{car}/setup')
+            ->group(function () {
+                Route::get('/', [SetupController::class, 'index'])->name('index');
+                Route::get('/{setup}', [SetupController::class, 'show'])->name('show');
+                Route::get('/{setup}/edit', [SetupController::class, 'edit'])->name('edit');
+                Route::post('/', [SetupController::class, 'store'])->name('store');
+                Route::put('/{setup}', [SetupController::class, 'update'])->name('update');
+                Route::delete('/{setup}', [SetupController::class, 'destroy'])->name('destroy');
+            })->name('setup.');
+            */
         })->name('garage.');
 });
 
