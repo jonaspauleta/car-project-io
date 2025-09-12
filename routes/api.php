8bfd1bc7005e93ab4c841534ac192a9b76c4cd12
João Paulo Santos <jonaspauleta2@gmail.com>
2025-09-12 19:40:10 +0100

    run pint

--- routes/api.php
+++ routes/api.php
@@ -8,32 +8,32 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
-->name('api.')
-->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->name('user');
-        
-    Route::prefix('cars')
-        ->name('cars.')
-        ->group(function () {
-            Route::get('/', [CarController::class, 'index'])->name('index');
-            Route::get('/{car}', [CarController::class, 'show'])->name('show');
-            Route::post('/', [CarController::class, 'store'])->name('store');
-            Route::put('/{car}', [CarController::class, 'update'])->name('update');
-            Route::delete('/{car}', [CarController::class, 'destroy'])->name('destroy');
+    ->name('api.')
+    ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->name('user');
 
-            Route::prefix('{car}/modifications')
-                ->name('modifications.')
-                ->group(function () {
-                    Route::get('/', [ModificationController::class, 'index'])->name('index');
-                    Route::get('/{modification}', [ModificationController::class, 'show'])->name('show');
-                    Route::post('/', [ModificationController::class, 'store'])->name('store');
-                    Route::put('/{modification}', [ModificationController::class, 'update'])->name('update');
-                    Route::delete('/{modification}', [ModificationController::class, 'destroy'])->name('destroy');
-                });
+        Route::prefix('cars')
+            ->name('cars.')
+            ->group(function () {
+                Route::get('/', [CarController::class, 'index'])->name('index');
+                Route::get('/{car}', [CarController::class, 'show'])->name('show');
+                Route::post('/', [CarController::class, 'store'])->name('store');
+                Route::put('/{car}', [CarController::class, 'update'])->name('update');
+                Route::delete('/{car}', [CarController::class, 'destroy'])->name('destroy');
 
-            /* TODO ideas for later
+                Route::prefix('{car}/modifications')
+                    ->name('modifications.')
+                    ->group(function () {
+                        Route::get('/', [ModificationController::class, 'index'])->name('index');
+                        Route::get('/{modification}', [ModificationController::class, 'show'])->name('show');
+                        Route::post('/', [ModificationController::class, 'store'])->name('store');
+                        Route::put('/{modification}', [ModificationController::class, 'update'])->name('update');
+                        Route::delete('/{modification}', [ModificationController::class, 'destroy'])->name('destroy');
+                    });
+
+                /* TODO ideas for later
             Route::prefix('{car}/maintenance')
             ->group(function () {
                 Route::get('/', [MaintenanceController::class, 'index'])->name('index');
@@ -52,5 +52,5 @@ Route::middleware('auth:sanctum')
                 Route::delete('/{setup}', [SetupController::class, 'destroy'])->name('destroy');
             })->name('setup.');
             */
-        });
-});
+            });
+    });
